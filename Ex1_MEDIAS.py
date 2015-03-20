3d3ee5a5e33a8f6b1e6f63a99fc8db951827d768
gabrielmbob <bobgab21@gmail.com>
2015-03-20 09:41:49 -0300

    Create Ex1_MEDIAS.py

---
+++ Ex1_MEDIAS.py
@@ -0,0 +1,8 @@
+def Media(x,y,z):
+    media = (x+y+z)/3
+    return media
+
+a = float(raw_input('Digite a primeira nota: '))
+b = float(raw_input('Digite a segunta nota: '))
+c = float(raw_input('Digite a terceira nota: '))
+print Media(a,b,c)
