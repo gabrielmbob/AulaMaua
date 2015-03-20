ae0f766d28345b098188e53223a225130fefae13
gabrielmbob <bobgab21@gmail.com>
2015-03-20 11:25:02 -0300

    Create Ex3_Medias2.py

---
+++ Ex3_Medias2.py
@@ -0,0 +1,20 @@
+listanotas=[]
+nota = 0
+
+def media(lista):
+    i = 0
+    soma = 0
+    for i in range (len(lista)):
+        soma = soma + lista[i]
+    if (len(lista)>0):
+        media = soma/len(lista)
+    else:
+        print 'Voce nao digitou uma nota'
+        media = 0
+    return media
+    
+
+while (nota >= 0):
+    nota = int(raw_input('Digite a nota: '))
+    listanotas.append(nota)
+print ('A media e: {0}'.format (media(listanotas[:-1])))
